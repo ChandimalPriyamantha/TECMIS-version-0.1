bb6915fa8e2e8c301fd03961dd9cdc09a389d415
M.P.C PRIYAMANTHA <95483538+ChandimalPriyamantha@users.noreply.github.com>
2023-03-22 13:25:06 +0530

    changes

---
+++ TECMIS_new_version/src/com/tecmis/B05/attendence/Attendence.java
@@ -0,0 +1,13 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.tecmis.B05.attendence;
+
+/**
+ *
+ * @author Vivobook
+ */
+public class Attendence {
+    
+}
